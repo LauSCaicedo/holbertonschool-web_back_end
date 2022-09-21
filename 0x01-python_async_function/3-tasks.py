39c555c5695b4f656409002f34c4155b61077b48
LauSCaicedo <3383@holbertonschool.com>
2022-09-20 23:32:27 -0500

    Add 3 task files

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Asyncio module and import 1 task file. """
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    Write a function task_wait_random that takes an
+    integer max_delay and returns a asyncio.Task.
+    """
+    task = asyncio.ensure_future(wait_random(max_delay))
+    return task
