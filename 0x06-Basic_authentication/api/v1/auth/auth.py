f242e8449bb7ecb5982b7acb11bd3695ee8a9ede
LauSCaicedo <3383@holbertonschool.com>
2022-10-04 23:06:32 -0500

    Updates project file

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,16 @@ class Auth():
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Returns False - path and excluded_paths. """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or not len(excluded_paths):
+            return True
+        if path[-1:] != '/':
+            path += '/'
+        if path in excluded_paths:
+            return False
+        else:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """ Returns None - request will be the Flask request object. """
